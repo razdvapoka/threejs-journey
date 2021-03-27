70620bb04732f83a5bca8ad16f83ae7eb89ff00d
razdvapoka <razdvapoka@gmail.com>
2021-03-27 16:35:46 +0300

    :sparkles: Add lesson 19

---
+++ 19-raycaster/src/script.js
@@ -0,0 +1,178 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Objects
+ */
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object1.position.x = -2;
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: "#ff0000" })
+);
+object3.position.x = 2;
+
+const raycaster = new THREE.Raycaster();
+// const rayOrigin = new THREE.Vector3(-3, 0, 0);
+// const rayDirection = new THREE.Vector3(10, 0, 0);
+// rayDirection.normalize();
+// raycaster.set(rayOrigin, rayDirection);
+// const intersect = raycaster.intersectObject(object2);
+// console.log(intersect);
+// const intersects = raycaster.intersectObjects([object1, object2, object3]);
+// console.log(intersects);
+
+scene.add(object1, object2, object3);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const mouse = new THREE.Vector2();
+window.addEventListener("mousemove", (e) => {
+  mouse.x = (e.clientX / sizes.width) * 2 - 1;
+  mouse.y = -((e.clientY / sizes.height) * 2 - 1);
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+let currentIntersect = null;
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+window.addEventListener("click", () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        object1.clicked = !object1.clicked;
+        object1.material.color.set("magenta");
+        break;
+      case object2:
+        object2.clicked = !object2.clicked;
+        object2.material.color.set("magenta");
+        break;
+      case object3:
+        object3.clicked = !object3.clicked;
+        object3.material.color.set("magenta");
+        break;
+    }
+  }
+});
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  object1.position.y = Math.sin(elapsedTime * 2);
+  object2.position.y = Math.sin(elapsedTime * 2 + 2);
+  object3.position.y = Math.sin(elapsedTime * 2 + 4);
+
+  raycaster.setFromCamera(mouse, camera);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  objectsToTest.forEach((object) => {
+    if (!object.clicked) {
+      object.material.color.set("red");
+    }
+  });
+
+  intersects.forEach(({ object }) => {
+    if (!object.clicked) {
+      object.material.color.set("green");
+    }
+  });
+
+  if (intersects.length) {
+    if (currentIntersect === null) {
+      console.log("mouseenter");
+      document.body.style.cursor = "pointer";
+    }
+    currentIntersect = intersects[0];
+  } else {
+    if (currentIntersect !== null) {
+      console.log("mouseleave");
+      document.body.style.cursor = "default";
+    }
+    currentIntersect = null;
+  }
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
